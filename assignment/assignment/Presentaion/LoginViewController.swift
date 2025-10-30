d11de3b1a0a9e073e629d9f0e1efcdeaf1948857
hyeyeonie <shy020916@naver.com>
2025-10-31 00:58:20 +0900

    [init] 초기 설정

---
+++ assignment/assignment/Presentaion/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  assignment
+//
+//  Created by 신혜연 on 10/25/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Setup Methods
+}
