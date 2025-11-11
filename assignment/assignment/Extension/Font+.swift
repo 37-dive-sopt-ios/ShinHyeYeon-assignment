55185ef6ca5eb1b9a3e6a147694ecb1827882217
hyeyeonie <shy020916@naver.com>
2025-11-11 19:35:05 +0900

    [add] head_b_16 폰트 추가

--- assignment/assignment/Extension/Font+.swift
+++ assignment/assignment/Extension/Font+.swift
@@ -10,6 +10,7 @@ import UIKit
 extension UIFont {
     static let head_b_24: UIFont = UIFont(name: "Pretendard-Bold", size: 24) ?? .systemFont(ofSize: 24, weight: .bold)
     static let head_b_18: UIFont = UIFont(name: "Pretendard-Bold", size: 18) ?? .systemFont(ofSize: 18, weight: .bold)
+    static let head_b_16: UIFont = UIFont(name: "Pretendard-Bold", size: 16) ?? .systemFont(ofSize: 16, weight: .bold)
     static let title_sb_18: UIFont = UIFont(name: "Pretendard-SemiBold", size: 18) ?? .systemFont(ofSize: 18, weight: .semibold)
     static let body_r_14: UIFont = UIFont(name: "Pretendard-Regular", size: 14) ?? .systemFont(ofSize: 14, weight: .regular)
     static let caption_r_10: UIFont = UIFont(name: "Pretendard-Regular", size: 10) ?? .systemFont(ofSize: 10, weight: .regular)
